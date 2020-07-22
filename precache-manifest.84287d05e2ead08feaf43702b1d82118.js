1641609c01728ea0d4725638ee2b2ced9f2f268a
tejas raibagi <tejasraibagi@gmail.com>
2020-07-22 13:02:10 +0530

    Updates

---
+++ precache-manifest.84287d05e2ead08feaf43702b1d82118.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1fe59b2a7ba0165d9f28163903d56a",
+    "url": "/career/index.html"
+  },
+  {
+    "revision": "0857c4e0d302e87c1949",
+    "url": "/career/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f2d4b8e6886173cc60e5",
+    "url": "/career/static/css/main.7ec892e7.chunk.css"
+  },
+  {
+    "revision": "0857c4e0d302e87c1949",
+    "url": "/career/static/js/2.e0f21af7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/career/static/js/2.e0f21af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d4b8e6886173cc60e5",
+    "url": "/career/static/js/main.ce457924.chunk.js"
+  },
+  {
+    "revision": "e97a7a70af5fc02a7fae",
+    "url": "/career/static/js/runtime-main.d9448256.js"
+  },
+  {
+    "revision": "f2e4e6ab4476d8896650201df1738b35",
+    "url": "/career/static/media/Web_bg.f2e4e6ab.png"
+  },
+  {
+    "revision": "0044911db8f2c32a100cfcba72d01b08",
+    "url": "/career/static/media/pattern.0044911d.png"
+  },
+  {
+    "revision": "fad906573173fb8884b0321bab64f1eb",
+    "url": "/career/static/media/phyerwaal_dev_logo_final.fad90657.png"
+  }
+]);
