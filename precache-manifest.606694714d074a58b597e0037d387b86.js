e589f6dd593fc5e5ebd1290e078ef61c50e465ed
tejas raibagi <tejasraibagi@gmail.com>
2020-07-30 17:42:30 +0530

    Updates

--- precache-manifest.606694714d074a58b597e0037d387b86.js
+++ precache-manifest.606694714d074a58b597e0037d387b86.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ea917135ebdccda572ba0bc3843e765",
+    "revision": "ac13fbd97c1ba92e01d42077a7e537ab",
     "url": "/career/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/career/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "f73b1bc91cac6233b640",
+    "revision": "b455aa0c1c282c62a0d3",
     "url": "/career/static/css/main.bce0224d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/career/static/js/2.06b1fc05.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f73b1bc91cac6233b640",
-    "url": "/career/static/js/main.7f5307e3.chunk.js"
+    "revision": "b455aa0c1c282c62a0d3",
+    "url": "/career/static/js/main.1e6b20b4.chunk.js"
   },
   {
     "revision": "e97a7a70af5fc02a7fae",
