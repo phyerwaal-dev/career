db071ec84d47bf3bd71413c0e911fbe723a0e599
sujoydev99 <sujoydev99@gmail.com>
2020-08-03 19:01:49 +0530

    pagination added

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,234 +1,325 @@
 import React from "react";
-import { Form } from 'react-bootstrap';
+import { Form } from "react-bootstrap";
 import {
-    MDBBtn,
-    MDBCard,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBRow,
-    MDBView,
-    MDBContainer,
-    MDBNavbar,
-    MDBNavbarBrand,
-    MDBNavbarToggler,
-    MDBCollapse,
-    MDBNavbarNav,
-    MDBNavItem,
-    MDBNavLink,
-    MDBInput,
-    MDBLink,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBRow,
+  MDBView,
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBInput,
+  MDBLink,
 } from "mdbreact";
-import axios from 'axios';
+import axios from "axios";
 import "../question.css";
 import questions from "../data/questions.json";
 import options from "../data/options.json";
 import logo from "../Web_bg.png";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class Questions extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            loading: false,
-            collapsed: false,
-            msg: "",
-            error: 0,
-            selection: questions.map((i, key) => {
-                let data = { id: i.id, question: i.question, val: null, select: null };
-                return data;
-            }),
-        };
-    }
-
-
-
-
-    handleTogglerClick = () => {
-        const { collapsed } = this.state;
-        this.setState({
-            collapsed: !collapsed,
-        });
+    this.state = {
+      current: [0, 1, 2, 3, 4],
+      loading: false,
+      collapsed: false,
+      msg: "",
+      error: 0,
+      selection: questions.map((i, key) => {
+        let data = { id: i.id, question: i.question, val: null, select: null };
+        return data;
+      }),
     };
+  }
 
-    onClick = (nr) => () => {
-        this.setState({
-            radio: nr,
-        });
-    };
+  handleTogglerClick = () => {
+    const { collapsed } = this.state;
+    this.setState({
+      collapsed: !collapsed,
+    });
+  };
 
-    componentDidMount() {
-        document.querySelector("nav").style.height = "65px";
+  onClick = (nr) => () => {
+    this.setState({
+      radio: nr,
+    });
+  };
+  componentWillMount() {
+    var xxx = this.shuffle(this.state.selection);
+    console.log(xxx);
+    this.setState({ selection: xxx });
+  }
+
+  componentDidMount() {
+    document.querySelector("nav").style.height = "65px";
+  }
+
+  componentWillUnmount() {
+    document.querySelector("nav").style.height = "auto";
+  }
+
+  shuffle(arra1) {
+    var ctr = arra1.length,
+      temp,
+      index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+      // And swap the last element with it
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
     }
+    return arra1;
+  }
 
-    componentWillUnmount() {
-        document.querySelector("nav").style.height = "auto";
-    }
+  //Set Answers
+  onChoose = async (id, x, val) => {
+    // console.log(this.state.selection);
 
-    //Set Answers
-    onChoose = async (val, e) => {
-        // console.log(this.state.selection);
-        let data = { id: parseInt(e.target.id), val: val, question: this.state.selection[parseInt(e.target.id) - 1].question, select: options[val].opt };
-        // console.log(data);
-        var new_selection = [];
-
-        await this.state.selection.map((i, key) => {
-            i.id == data.id
-                ? new_selection.push(data)
-                : new_selection.push({ id: i.id, val: i.val, question: i.question, select: i.select });
-        });
-        await this.setState({ selection: new_selection });
+    console.log(id, x, val);
+    let data = {
+      id: parseInt(id),
+      val: val,
+      question: this.state.selection[x].question,
+      select: options[val].opt,
     };
+    console.log(data);
+    var new_selection = [];
 
-    //Validate questions
-    validate = async () => {
-        await this.setState({
-            error: 0,
-            msg: ""
+    await this.state.selection.map((i, key) => {
+      if (i.id === data.id) console.log(i);
+      i.id == data.id
+        ? new_selection.push(data)
+        : new_selection.push({
+            id: i.id,
+            val: i.val,
+            question: i.question,
+            select: i.select,
+          });
+    });
+    await this.setState({ selection: new_selection });
+    console.log(this.state.selection);
+  };
+
+  //Validate questions
+  validate = async () => {
+    await this.setState({
+      error: 0,
+      msg: "",
+    });
+    var err = 0;
+    for (var i = 0; i <= this.state.selection.length - 1; i++) {
+      if (this.state.selection[i].val == null) {
+        // console.log(this.state.selection[i].id + " : " + this.state.selection[i].val);
+        // await this.setState({ msg: "" });
+        // this.setState({ msg: "Please fill all the questions." });
+        // this.setState({ error: this.state.error + 1 });
+        err = err + 1;
+      }
+      // else {
+      //     console.log(" Value is Null for " + i);
+      //     console.log(this.state.selection);
+      //     await this.setState({ msg: "Please fill all the questions." });
+      //     await this.setState({ error: this.state.error + 1 });
+      // }
+    }
+    // if
+    // console.log(
+    //   this.state.selection.sort(function (a, b) {
+    //     return a.id - b.id;
+    //   })
+    // );
+    if (err > 0)
+      this.setState({
+        error: err,
+        msg: "Please fill all the questions.",
+      });
+    console.log("Error: " + this.state.error);
+    if (this.state.error == 0) {
+      axios
+        .post("https://phyerwaal-dev-career.herokuapp.com/submit-data", {
+          data: this.state.selection.sort(function (a, b) {
+            return a.id - b.id;
+          }),
         })
-        for (var i = 0; i <= this.state.selection.length - 1; i++) {
-            if (this.state.selection[i].val == null) {
-                // console.log(this.state.selection[i].id + " : " + this.state.selection[i].val);
-                // await this.setState({ msg: "" });
-                this.setState({ msg: "Please fill all the questions." });
-                this.setState({ error: this.state.error + 1 });
-            }
-            // else {
-            //     console.log(" Value is Null for " + i);
-            //     console.log(this.state.selection);
-            //     await this.setState({ msg: "Please fill all the questions." });
-            //     await this.setState({ error: this.state.error + 1 });
-            // }
-        }
-        console.log("Error: " + this.state.error);
-        if (this.state.error == 0) {
-            axios.post('https://phyerwaal-dev-career.herokuapp.com/submit-data', {
-                data: this.state.selection
-            }).then((response) => {
-                // console.log(response.data);
-                // console.log("This :" + this);
-                this.props.storeResponse(response.data);
-                this.props.history.push('/career/results');
-            }).catch(function (error) {
-                //console.log(error);
-            });
-        }
+        .then((response) => {
+          // console.log(response.data);
+          // console.log("This :" + this);
+          this.props.storeResponse(response.data);
+          this.props.history.push("/career/results");
+        })
+        .catch(function (error) {
+          //console.log(error);
+        });
     }
+  };
 
-    render() {
-        const { collapsed } = this.state;
-        const navStyle = { marginTop: "2rem" };
-        const overlay = (
-            <div
-                id="sidenav-overlay"
-                style={{ backgroundColor: "white" }}
-                onClick={this.handleTogglerClick}
-            />
-        );
-        return (
-            <div id="questions">
-                <div className="fadeInDown">
-                    <MDBNavbar
-                        color="secondary-color"
-                        style={navStyle}
-                        light
-                        expand="md"
-                        scrolling
-                        transparent
-                    >
-                        <MDBContainer>
-                            <MDBNavbarBrand href="/career/">
-                                <strong>APSIT Career</strong>
-                            </MDBNavbarBrand>
-                            <MDBNavbarToggler onClick={this.handleTogglerClick} />
-                            <MDBCollapse isOpen={collapsed} navbar>
-                                <MDBNavbarNav right>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/career/">Home</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to="/career/dev">Developers</MDBNavLink>
-                                    </MDBNavItem>
-                                </MDBNavbarNav>
-                            </MDBCollapse>
-                        </MDBContainer>
-                    </MDBNavbar>
-                    {collapsed && overlay}
-                </div>
-                <MDBView src={logo}>
-                    <MDBContainer
-                        style={{ height: "100%", width: "100%", paddingTop: "5rem" }}
-                        className="d-flex justify-content-center align-items-center"
-                    >
-                        <MDBRow>
-                            <h1
-                                className="display-10 font-weight-bold question-header fadeInDown" style={{ animationDelay: ".2s" }}
-                            >
-                                QUESTIONS
+  render() {
+    const { collapsed } = this.state;
+    const navStyle = { marginTop: "2rem" };
+    const overlay = (
+      <div
+        id='sidenav-overlay'
+        style={{ backgroundColor: "white" }}
+        onClick={this.handleTogglerClick}
+      />
+    );
+    return (
+      <div id='questions'>
+        <div className='fadeInDown'>
+          <MDBNavbar
+            color='secondary-color'
+            style={navStyle}
+            light
+            expand='md'
+            scrolling
+            transparent
+          >
+            <MDBContainer>
+              <MDBNavbarBrand href='/career/'>
+                <strong>APSIT Career</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.handleTogglerClick} />
+              <MDBCollapse isOpen={collapsed} navbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink to='/career/'>Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to='/career/dev'>Developers</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+          {collapsed && overlay}
+        </div>
+        <MDBView src={logo}>
+          <MDBContainer
+            style={{ height: "100%", width: "100%", paddingTop: "5rem" }}
+            className='d-flex justify-content-center align-items-center'
+          >
+            <MDBRow>
+              <h1
+                className='display-10 font-weight-bold question-header fadeInDown'
+                style={{ animationDelay: ".2s" }}
+              >
+                QUESTIONS
               </h1>
-                            <MDBCard style={{ width: "100%" }}>
-                                {questions.map((question, index) => {
-                                    return (
-                                        <div
-                                            className="card fadeInDown"
-                                            style={{ animationDelay: ".4s" }}
-                                        >
-                                            <MDBCardBody>
-                                                <MDBCardTitle>
-                                                    Q{index + 1}. {question.question}
-                                                </MDBCardTitle>
-                                                {options.map((opt, oindex) => {
-                                                    return (
-                                                        <MDBContainer className="mt-3">
-                                                            <MDBInput key={oindex}
-                                                                required
-                                                                type="radio"
-                                                                label={opt.opt}
-                                                                id={question.id}
-                                                                onClick={this.onChoose.bind(
-                                                                    question.id,
+              <MDBCard style={{ width: "100%" }}>
+                {this.state.selection.map((question, index) => {
+                  return (
+                    <div
+                      className='card fadeInDown'
+                      style={
+                        this.state.current.includes(index)
+                          ? { display: "block", animationDelay: ".0s" }
+                          : { display: "none", animationDelay: ".0s" }
+                      }
+                    >
+                      <MDBCardBody>
+                        <MDBCardTitle>
+                          Q{index + 1}. {question.question}
+                        </MDBCardTitle>
+                        {options.map((opt, oindex) => {
+                          return (
+                            <MDBContainer className='mt-3'>
+                              <MDBInput
+                                key={oindex}
+                                required
+                                type='radio'
+                                label={opt.opt}
+                                id={question.id}
+                                onClick={(e) => {
+                                  this.onChoose(e.target.id, index, opt.val);
+                                }}
+                                checked={
+                                  this.state.selection[index].val == opt.val
+                                    ? true
+                                    : false
+                                }
+                              />
+                            </MDBContainer>
+                          );
+                        })}
+                      </MDBCardBody>
+                      <hr></hr>
+                    </div>
+                  );
+                })}
+              </MDBCard>
+              <div
+                className='send-btn fadeInDown'
+                style={{ animationDelay: "1s" }}
+              >
+                <h5 className='error-msg justify-content-center align-items-center'>
+                  {this.state.msg}
+                </h5>
+                {this.state.current[4] <= 4 ? null : (
+                  <MDBBtn
+                    color='success'
+                    onClick={() => {
+                      this.setState({
+                        current: Array.from(this.state.current, (x) => x - 5),
+                      });
+                    }}
+                  >
+                    prev
+                  </MDBBtn>
+                )}
 
-                                                                    opt.val,
-                                                                )}
-                                                                checked={
-                                                                    this.state.selection[index].val == opt.val
-                                                                        ? true
-                                                                        : false
-                                                                }
-                                                            />
-                                                        </MDBContainer>
-                                                    );
-                                                })}
-                                            </MDBCardBody>
-                                            <hr></hr>
-                                        </div>
-                                    );
-                                })}
-                            </MDBCard>
-                            <div
-                                className="send-btn fadeInDown"
-                                style={{ animationDelay: "1s" }}
-                            >
-                                <h5 className="error-msg justify-content-center align-items-center">{this.state.msg}</h5>
-                                <MDBBtn color="success" onClick={this.validate}>SEND ANSWER</MDBBtn>
+                {this.state.current[4] >= this.state.selection.length - 1 ? (
+                  <MDBBtn color='success' onClick={this.validate}>
+                    SEND ANSWER
+                  </MDBBtn>
+                ) : (
+                  <MDBBtn
+                    color='success'
+                    onClick={async () => {
+                      await this.setState({
+                        current: Array.from(this.state.current, (x) => x + 5),
+                      });
+                      console.log(this.state.current);
+                    }}
+                  >
+                    next
+                  </MDBBtn>
+                )}
 
-
-                            </div>
-                        </MDBRow>
-                    </MDBContainer>
-                </MDBView>
-            </div>
-        );
-    }
+                {/* <MDBBtn color='success' onClick={this.validate}>
+                  SEND ANSWER
+                </MDBBtn> */}
+              </div>
+            </MDBRow>
+          </MDBContainer>
+        </MDBView>
+      </div>
+    );
+  }
 }
 
 //Dispatch Data to store state
 const mapDispatchToProps = (dispatch) => {
-    return {
-        storeResponse: (response) => { dispatch({ type: 'RES_SUCCESS', response: response }) },
-    }
-}
-
+  return {
+    storeResponse: (response) => {
+      dispatch({ type: "RES_SUCCESS", response: response });
+    },
+  };
+};
 
 export default connect(null, mapDispatchToProps)(Questions);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
-import rootReducer from './Reducer/index';
-import thunk from 'redux-thunk';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import rootReducer from "./Reducer/index";
+import thunk from "redux-thunk";
 
 const middleware = [thunk];
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
-const store = createStore(rootReducer, compose(
-  applyMiddleware(...middleware),
-)
-)
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+  // other store enhancers if any
+);
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
